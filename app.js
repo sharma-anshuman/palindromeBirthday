b6e73894ea486c451039bccbf7110242786df24c
Anshuman Sharma <code4anshu@gmail.com>
2022-10-05 15:53:05 +0530

    update: app.js
    logic improved

--- app.js
+++ app.js
@@ -2,19 +2,48 @@ var dateInput = document.querySelector('#ip-date');
 var resBtn = document.querySelector('.btn');
 var outputText = document.querySelector('.op-text');
 
-// DD-MM-YYYY
-// MM-DD-YYYY
-// YYYY-MM-DD
-// DD-MM-YY
-// MM-DD-YY
-// YY-MM-DD
+var nDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-function getDateFormats(){
-    var dt = dateInput.value;
-    var date = dt.toString();
-    var dateLst = date.split('-');
-    dateLst.reverse();
-    console.log(dateLst);
+var dateObj = { day: 1, month: 1, year: 2001 };
+
+ function isLeap(year){
+    if (year % 400 == 0)return true;
+    if (year % 100 == 0)return false;
+    if (year % 4 == 0)return true;
+    return false;
+ }
+
+ function incDate(date){
+    date.day++;
+    if(nDays[date.month - 1] < date.day){
+        if(date.month == 2){
+            if(isLeap(date.year)){
+                return date;
+            }
+            else{
+                date.day = 1;
+                date.month++;
+                return date;
+            }
+        }
+        else{
+            date.day = 1;
+            date.month++;
+        }
+    }
+    if(date.month>12){
+        date.month = 1;
+        date.year++;
+    }
+    return date;
+}
+
+function getDateFormats(date){
+    var day = date.day, month = date.month, year = date.year;
+    if(day<10) day = '0'+day;
+    if(month<10) month = '0'+month;
+    day = day.toString(), month = month.toString(), year = year.toString();
+    var dateLst = [day, month, year];
     var formatList = [];
     formatList.push(dateLst.join(''));
     formatList.push(dateLst[1]+dateLst[0]+dateLst[2]);
@@ -25,23 +54,20 @@ function getDateFormats(){
     return formatList;
 }
 
-
 function isPalindrome(date){
-    var revDate = date.split('').reverse().join('');
-    if(revDate === date){
-        return true;
-    }
-    else{
-        return false;
-    }
+    var x = date;
+    var revDate = x.split('').reverse().join('');
+    if(revDate === date)return true;
+    else return false;
 }
 
-
 function clickHandler(){
-    var dateFormats = getDateFormats();
-    for(var i = 0; i<dateFormats.length; i++){
-        console.log(dateFormats[i]);
-    }
+    var dt = dateInput.value;
+    var date = dt.toString();
+    var dateLst = date.split('-');
+    dateLst.reverse();
+    dateObj.day = Number(dateLst[0]), dateObj.month = Number(dateLst[1]), dateObj.year = Number(dateLst[2]);
+    var dateFormats = getDateFormats(dateObj);
     var flag = false;
     for(var i = 0; i<6; i++){
         if(isPalindrome(dateFormats[i])){
@@ -53,7 +79,21 @@ function clickHandler(){
         outputText.innerHTML = "Congratulations!! Your Birthday is a palindrome :)";
     }
     else{
-        console.log("Sad");
+        flag = false;
+        var ct = 0;
+        while(!flag){
+            ct++;
+            dateObj = incDate(dateObj);
+            dateFormats = getDateFormats(dateObj);
+            for(var i = 0; i<6; i++){
+                if(isPalindrome(dateFormats[i])){
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        console.log(dateObj);
+        outputText.innerHTML = "Oops you missed by " + ct + " days, next palindrome date is: " + dateObj.day+'-'+dateObj.month+'-'+dateObj.year;
     }
 }
 
